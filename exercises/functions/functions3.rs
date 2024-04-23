8a15797030610c8c29083d987d819f8fbcd67c56
徐家宝 <147332724+ClarkVon@users.noreply.github.com>
2024-04-23 10:38:17 +0000

    modified:   exercises/functions/functions1.rs 	modified:   exercises/functions/functions2.rs 	modified:   exercises/functions/functions3.rs 	modified:   exercises/functions/functions4.rs 	modified:   exercises/functions/functions5.rs 	modified:   exercises/if/if1.rs 	modified:   exercises/if/if2.rs 	modified:   exercises/if/if3.rs     modified:   exercises/intro/intro2.rs 	modified:   exercises/primitive_types/primitive_types1.rs 	modified:   exercises/primitive_types/primitive_types2.rs 	modified:   exercises/primitive_types/primitive_types3.rs 	modified:   exercises/primitive_types/primitive_types4.rs 	modified:   exercises/primitive_types/primitive_types5.rs 	modified:   exercises/primitive_types/primitive_types6.rs	modified:   exercises/quiz1.rs 	modified:   exercises/variables/variables1.rs 	modified:   exercises/variables/variables2.rs 	modified:   exercises/variables/variables3.rs 	modified:   exercises/variables/variables4.rs 	modified:   exercises/variables/variables5.rs 	modified:   exercises/variables/variables6.rs

--- exercises/functions/functions3.rs
+++ exercises/functions/functions3.rs
@@ -3,14 +3,14 @@
 // Execute `rustlings hint functions3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    call_me();
+    call_me(3);
 }
 
 fn call_me(num: u32) {
-    for i in 0..num {
+    for i in (0..num) {
         println!("Ring! Call number {}", i + 1);
     }
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (a: i32)-> i32{
+    if a <= 40{
+        a*2
+    }else{
+        a
+    }
+
+}
 
 // Don't modify this function!
 #[test]
