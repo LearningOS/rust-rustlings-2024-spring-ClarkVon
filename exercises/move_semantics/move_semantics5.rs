4ae20c787f659bcee671930b019f23e4cf5359a9
徐家宝 <147332724+ClarkVon@users.noreply.github.com>
2024-04-25 07:12:45 +0000

    modified:   exercises/move_semantics/move_semantics1.rs 	modified:   exercises/move_semantics/move_semantics2.rs 	modified:   exercises/move_semantics/move_semantics3.rs 	modified:   exercises/move_semantics/move_semantics4.rs 	modified:   exercises/move_semantics/move_semantics5.rs 	modified:   exercises/move_semantics/move_semantics6.rs 	modified:   exercises/structs/structs1.rs 	modified:   exercises/vecs/vecs1.rs 	modified:   exercises/vecs/vecs2.rs

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -6,13 +6,14 @@
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let mut x = 100;
-    let y = &mut x;
-    let z = &mut x;
-    *y += 100;
-    *z += 1000;
+    let mut y =  x;
+    y += 100;
+    let mut z = y;
+    z += 1000;
+    let x = z;
     assert_eq!(x, 1200);
 }
