3a8bd19a9d9a297b2176d0a12554924518df85d7
徐家宝 <147332724+ClarkVon@users.noreply.github.com>
2024-04-27 17:36:10 +0000

    modified:   exercises/enums/enums3.rs 	modified:   exercises/strings/strings1.rs 	modified:   exercises/strings/strings2.rs 	modified:   exercises/strings/strings3.rs

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,10 +5,13 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    Move(Point),
+    Echo(String),
+    ChangeColor(u8, u8, u8),
+    Quit
 }
 
 struct Point {
@@ -43,6 +46,20 @@ impl State {
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
+        use crate::Message::*;
+
+        match message{
+            Move(msg) => {self.position.x = msg.x;self.position.y = msg.y}
+            Echo(msg) => self.message = msg,
+ChangeColor(r, g, b) => {
+                self.color.0 = r;
+                self.color.1 = g;
+                self.color.2 = b;
+            }
+
+            Quit => self.quit = true
+            
+        }
     }
 }
 
--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,29 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    /* let s = String::from(input);
+    let new_str = s.trim();
+    new_str.to_string() */
+    let s = String::from(input).trim().to_string();
+    s
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut s = input.to_string();
+    s.push_str(" world!");
+    s
+
+    
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    // TODO: Replace "cars" in the string with "balloons"!  
+    let replaced_string = input.replace("cars", "balloons");
+    replaced_string
 }
 
 #[cfg(test)]
